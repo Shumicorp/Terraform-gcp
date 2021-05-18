322f9f7b1e1953c5cd0c50a73d531c412a2f7537
Shumicorp <Shumicorp@gmail.com>
2021-05-18 15:42:54 +0300

    Done(vps, sa, st, tm, ig, db, lb(http +https+ssl certificate), dns)

--- modules/lb/load-balancer.tf
+++ modules/lb/load-balancer.tf
@@ -37,6 +37,26 @@ resource "google_compute_url_map" "default" {
   default_service = google_compute_backend_service.wordpress-backend.id
 }
 
+resource "google_compute_target_https_proxy" "https-proxy" {
+  name             = "test-proxy"
+  url_map          = google_compute_url_map.default.id
+  ssl_certificates = [google_compute_managed_ssl_certificate.default.id]
+}
+resource "google_compute_managed_ssl_certificate" "default" {
+  name = "mrusn-ssl-terra"
+
+  managed {
+    domains = ["mrusn.pp.ua."]
+  }
+}
+
+resource "google_compute_global_forwarding_rule" "load-balancer-https" {
+  name                  = "https-role"
+  ip_address            = google_compute_global_address.wordpress-front.address
+  port_range            = "443"
+  target                = google_compute_target_https_proxy.https-proxy.id
+}
+
 output "front-ip" { # output id network outside for ather module
   value = [google_compute_global_address.wordpress-front.address]
 }
