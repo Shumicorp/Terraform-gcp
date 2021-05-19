1bd4ff2d0ec9a4f1c6b671c2281d3eafae036fe9
Shumicorp <Shumicorp@gmail.com>
2021-05-19 13:50:22 +0300

    - http front-service

--- modules/lb/load-balancer.tf
+++ modules/lb/load-balancer.tf
@@ -20,18 +20,6 @@ resource "google_compute_backend_service" "wordpress-backend" {
   
 }
 
-resource "google_compute_global_forwarding_rule" "load-balancer-rule" {
-  name                  = "wordpress-forwarding-rule"
-  ip_address            = google_compute_global_address.wordpress-front.address
-  port_range            = "80"
-  target                = google_compute_target_http_proxy.http-proxy.id
-}
-
-resource "google_compute_target_http_proxy" "http-proxy" {
-  name    = "wordpress-proxy"
-  url_map = google_compute_url_map.default.id
-}
-
 resource "google_compute_url_map" "default" {
   name            = "wordpress-map"
   default_service = google_compute_backend_service.wordpress-backend.id
