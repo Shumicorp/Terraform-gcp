c5b739e3dde17aa9a50e7941856395db93fb4398
Shumicorp <Shumicorp@gmail.com>
2021-05-24 17:47:49 +0300

    edit service-account + add monitoring in start-script code

--- modules/service-account/service-account.tf
+++ modules/service-account/service-account.tf
@@ -1,4 +1,10 @@
 resource "google_service_account" "service-account" {
   account_id   = "wordpress-account"
-  display_name = "Wordpress Account"
+  display_name = "Wordpress Account" 
+}
+ 
+resource "google_project_iam_member" "project" {
+  project = "my-elk-wp-softserve"
+  role    = "roles/editor"
+  member  = "serviceAccount:${google_service_account.service-account.email}"
 }
